cfa197931f00fd22f75951931b8fa5494eee7874
Kuftin Vladislav <boer5678@gmail.com>
2022-01-26 00:07:41 +0300

    HW 4 DONE

--- ordersapp/urls.py
+++ ordersapp/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
 
 from ordersapp.views import OrderListView, OrderCreateView, OrderUpdateView, OrderDeleteView, OrderDetailView, \
-    order_forming_complete
+    order_forming_complete, get_product_price
 
 app_name = 'ordersapp'
 urlpatterns = [
 
     path('', OrderListView.as_view(), name='list'),
     path('create/', OrderCreateView.as_view(), name='create'),
-    path('uprate/<int:pk>/', OrderUpdateView.as_view(), name='update'),
+    path('update/<int:pk>/', OrderUpdateView.as_view(), name='update'),
     path('read/<int:pk>/', OrderDetailView.as_view(), name='read'),
     path('delete/<int:pk>/', OrderDeleteView.as_view(), name='delete'),
     path('forming_complete/<int:pk>/', order_forming_complete, name='forming_complete'),
+    path('product/<int:pk>/price/', get_product_price, name='get_product_price'),
 
 ]
